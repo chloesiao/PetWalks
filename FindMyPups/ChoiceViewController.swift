2a2fd658b0d167e2f91c825829a564184dad48c4
Chloe Siao <siaoc@d-e.org>
2018-05-02 10:55:19 -0400

    initial commit

---
+++ FindMyPups/ChoiceViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ChoiceViewController.swift
+//  FindMyPups
+//
+//  Created by iD Student on 8/11/17.
+//  Copyright © 2017 iD Tech. All rights reserved.
+//
+//
+//import UIKit
+//
+//class ChoiceViewController: UIViewController {
+//    @IBOutlet weak var searchBar: UITextField!
+//    let dataStore = LocationDataStore()
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Do any additional setup after loading the view.
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+//    
+//    @IBAction func createdOwnLocationTapped(_ sender: Any) {
+//            dataStore.nearString = searchBar.text
+//        
+//    }
+//
+//    @IBAction func usingOwnLocationTapped(_ sender: Any) {
+//        dataStore.ll = true
+//        //we need to make sure that currentlocationn isn't nil
+//    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
---
+++ Podfile
@@ -0,0 +1,25 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.2'
+
+pod 'Cluster', '~> 1.0'
+
+target 'FindMyPups' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  
+  # Pods for FindMyPups
+
+  target 'FindMyPupsTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'FindMyPupsUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+  pod 'Alamofire'
+  pod 'SwiftyJSON'
+
+end
